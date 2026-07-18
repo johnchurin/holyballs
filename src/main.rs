652cd61b69c492ecc5c1af219c0b38296726873a
John Churin <john.churin@gmail.com>
2026-07-17 21:59:42 -0700

    Physics working with sensors, and scorecard

---
+++ src/main.rs
@@ -0,0 +1,421 @@
+use bevy::input::common_conditions::input_just_pressed;
+use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+use bevy::color::palettes::basic::GREEN;
+use bevy::color::palettes::basic::WHITE;
+use bevy::color::palettes::basic::RED;
+use bevy::color::palettes::basic::BLUE;
+use rand::RngExt;
+use std::f32::consts::{FRAC_PI_2, PI};
+use std::fmt::format;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        // Initialize the Rapier physics engine and the debug renderer
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+//        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup_physics)
+        .add_systems(Update, cleanup_dead_balls)
+        .insert_resource(Selected{ entity: None, prev_entity: None })
+        .insert_resource(ScoreBoard::new())
+        .add_systems(Update, (
+            drop_a_ball.run_if(input_just_pressed(KeyCode::Enter)),
+            drop_a_ball.run_if(input_just_pressed(KeyCode::NumpadEnter)),
+            impulse_up.run_if(input_just_pressed(KeyCode::Space)),
+            impulse_up.run_if(input_just_pressed(KeyCode::Numpad5)),
+            impulse_left.run_if(input_just_pressed(KeyCode::ArrowLeft)),
+            impulse_left.run_if(input_just_pressed(KeyCode::Numpad4)),
+            impulse_right.run_if(input_just_pressed(KeyCode::ArrowRight)),
+            impulse_right.run_if(input_just_pressed(KeyCode::Numpad6)),
+            impulse_back.run_if(input_just_pressed(KeyCode::ArrowDown)),
+            impulse_back.run_if(input_just_pressed(KeyCode::Numpad2)),
+            impulse_forward.run_if(input_just_pressed(KeyCode::ArrowUp)),
+            impulse_forward.run_if(input_just_pressed(KeyCode::Numpad8)),
+            box_size.run_if(input_just_pressed(KeyCode::KeyB)),
+            cylinder_size.run_if(input_just_pressed(KeyCode::KeyC)),
+            handle_sensor_events,
+            propagate_color,
+            update_score.run_if(resource_changed::<ScoreBoard>),
+        ),
+    )
+    .add_observer(un_select)
+    .run();
+}
+#[derive(Component)]
+struct Fence {
+}
+
+#[derive(Component)]
+struct Post {
+}
+
+#[derive(Component, Clone, Copy, Debug)]
+struct BouncyBall {
+    color: Color,
+}
+
+#[derive(Component)]
+struct BouncyBallChild {
+}
+
+#[derive(Component)]
+struct SensorChild {
+}
+
+#[derive(Resource)]
+struct ScoreBoard {
+    score: i32,
+    round: i32,
+    total: i32,
+}
+
+impl ScoreBoard {
+    fn new() -> Self {
+        Self{score: 0, round: 0, total: 0}
+    }
+    fn hit(&mut self) {
+        self.score += 1;
+        self.total += 1;
+    }
+    fn new_round(&mut self) {
+        self.score = 0;
+        self.round += 1;
+    }
+    fn reset(&mut self) {
+        self.score = 0;
+        self.round = 0;
+        self.total = 0;
+    }
+}
+#[derive(Event)]
+struct Unselect {
+}
+
+#[derive(Resource)]
+struct Selected {
+    entity: Option<Entity>,
+    prev_entity: Option<Entity>,
+}
+
+fn handle_sensor_events(
+    mut messages: ResMut<Messages<CollisionEvent>>,
+    mut scoreboard: ResMut<ScoreBoard>,
+) {
+    for event in messages.drain() {
+//        println!("handle_sensor_events");
+        match event {
+            CollisionEvent::Started(entity1, entity2, _flags) => {
+                scoreboard.hit();
+                println!("Something entered the sensor: {:?} and {:?}", entity1, entity2);
+            }
+            CollisionEvent::Stopped(entity1, entity2, _flags) => {
+                println!("Something left the sensor: {:?} and {:?}", entity1, entity2);
+            }
+        }
+    }
+}
+
+fn un_select(
+    _event: On<Unselect>,
+    mut query: Query<(&mut BouncyBall, Entity), With<BouncyBall>>,
+    mut selected : ResMut<Selected>,
+) {
+    for (mut bouncy_ball, entity) in &mut query {
+        if selected.prev_entity == Some(entity) {
+            bouncy_ball.color = Color::Srgba(Srgba::rgb(0.9, 0.0, 0.9));
+            selected.prev_entity = None;
+            break;
+        }
+    }
+}
+fn cleanup_dead_balls(
+    mut commands: Commands,
+    mut old_balls: Query<(Entity, &mut Transform), With<BouncyBall>>,
+    mut selected : ResMut<Selected>,
+) {
+    // cleanup old balls out of range
+    for (entity, ball) in old_balls.iter_mut() {
+        if ball.translation.y < -20.0 {
+            if selected.entity == Some(entity) {
+               selected.entity = None;
+            }
+            commands.entity(entity).despawn();
+//            println!("Ball despawned");
+        }
+    }
+}
+
+fn drop_a_ball(
+    mut old_balls: Query<(Entity, &mut Transform), With<BouncyBall>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut selected : ResMut<Selected>,
+    mut scoreboard: ResMut<ScoreBoard>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let mut rng = rand::rng();
+    let x_pos: f32 = rng.random_range(-12.0..0.);
+    let y_pos: f32 = rng.random_range(15.0..25.);
+    let z_pos: f32 = rng.random_range(-9.0..9.0);
+    // 4. Spawn a Dynamic Bouncing Ball
+    let bouncy_ball = BouncyBall {color: Color::from(RED)};
+
+    if keyboard_input.pressed(KeyCode::ShiftLeft)
+        || keyboard_input.pressed(KeyCode::ShiftRight)
+    {
+        scoreboard.reset();
+        selected.entity = None;
+        selected.prev_entity = None;
+        for (entity, ball) in old_balls.iter_mut() {
+            commands.entity(entity).despawn();
+        }
+        return;
+    } else {
+        scoreboard.new_round();
+    }
+    let entity = commands.spawn((
+        bouncy_ball,
+        RigidBody::Dynamic,
+        // Lower the Damping for a more advanced game
+        Damping {
+            linear_damping: 0.2,
+            angular_damping: 0.2,
+        },
+        Collider::ball(0.5),
+        // Adding restitution makes the ball bounce
+        Restitution::new(1.0),
+//        GravityScale(2.0),
+        ExternalImpulse::default(),
+        Transform::from_xyz(x_pos, y_pos, z_pos),
+        Velocity::linear(Vec3::new(2.0, 0.0, 0.0)),
+        Visibility::default(),
+    )).with_children(|parent| {
+        parent.spawn((
+                BouncyBallChild{},
+                Mesh3d(meshes.add(Mesh::from(Sphere::new(0.5)))),
+                MeshMaterial3d(materials.add(bouncy_ball.color)),
+            ));
+        }).id();
+    selected.prev_entity = selected.entity;
+    selected.entity = Some(entity);
+    commands.trigger(Unselect{});
+}
+
+fn box_size(
+    mut query: Query<(&mut Collider, &Mesh3d, &mut Transform), With<Fence>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let increment = if keyboard_input.pressed(KeyCode::ShiftLeft) || keyboard_input.pressed(KeyCode::ShiftRight)
+    {
+        0.4
+    } else {
+        -0.4
+    };
+    for (mut collider, mesh3d, mut transform) in query.iter_mut() {
+//        println!("Translation before {}", transform.translation.y);
+        let ext = collider.as_cuboid().unwrap().half_extents();
+        if ext.y < 0.1 && increment < 0.0 {return;}
+        if let Some(mut mesh) = meshes.get_mut(&mesh3d.0) {
+            // Reconstruct the base shape with the new dimensions
+            *collider = Collider::cuboid(ext.x, ext.y+increment, ext.z);
+            *mesh = Mesh::from(Cuboid::new(ext.x*2.0, (ext.y+increment)*2.0, ext.z*2.0));
+//            println!("mesh y incr: {}, ext.y: {}", increment, ext.y);
+        }
+        transform.translation.y += increment;
+//        println!("Translation y{}", transform.translation);
+    }
+}
+fn propagate_color(
+    parent_query: Query<(&BouncyBall, &Children), Changed<BouncyBall>>,
+    mut child_query: Query<(&MeshMaterial3d<StandardMaterial>), With<BouncyBallChild>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    for (bouncy_ball, children) in &parent_query {
+        // println!("propagate_color {:?}", bouncy_ball.color);
+        for &child in children {
+            // println!("propagate_color: in child {}", child);
+            let material_handle= child_query.get_mut(child).unwrap();
+            // Assign the parent color to the child
+            let mut material = materials.get_mut(material_handle).unwrap();
+            material.base_color = bouncy_ball.color;
+            //    println!("propagate_color set to: {:?} in {:?}", material.base_color, entity);
+        }
+    }
+}
+fn cylinder_size(
+    mut query: Query<(&mut Collider, &mut Mesh3d, &mut Transform), With<Post>>,
+    mut sensor_query: Query<(&mut Collider, &mut Transform), (With<SensorChild>, Without<Post>)>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let increment = if keyboard_input.pressed(KeyCode::ShiftLeft) || keyboard_input.pressed(KeyCode::ShiftRight)
+    {
+        0.5
+    } else {
+        -0.5
+    };
+    for (mut collider, mesh3d, mut transform) in query.iter_mut() {
+        println!("Translation before {}", transform.translation.y);
+        let h = collider.as_cylinder().unwrap().half_height();
+        if h < 0.1 && increment < 0.0 {return;}
+        let r = collider.as_cylinder().unwrap().radius();
+        if let Some(mut mesh) = meshes.get_mut(&mesh3d.0) {
+            // Reconstruct the base shape with the new dimensions
+            *collider = Collider::cylinder(h+increment,r);
+            *mesh = Mesh::from(Cylinder::new(r, (h*2.0)+increment*2.0));
+            println!("mesh y incr: {}, ext.y: {}", increment, h);
+        }
+//        transform.translation.y += increment;
+       *transform = Transform::from_xyz(transform.translation.x, transform.translation.y+increment, transform.translation.z);
+//        println!("h: {}, Translation y {}", h, transform.translation.y);
+    }
+    for (mut collider, mut transform) in sensor_query.iter_mut() {
+        let h = collider.as_cylinder().unwrap().half_height();
+        if h < 0.1 && increment < 0.0 {return;}
+        let r = collider.as_cylinder().unwrap().radius();
+        *collider = Collider::cylinder(h+increment,r);
+        *transform = Transform::from_xyz(transform.translation.x, transform.translation.y+increment, transform.translation.z);
+    }
+}
+
+fn impulse_up(
+    mut impulses: Query<&mut ExternalImpulse, With<BouncyBall>>,
+    selected: Res<Selected>,
+) {
+    if selected.entity.is_some() {
+        let mut impulse = impulses.get_mut(selected.entity.unwrap()).unwrap();
+        impulse.impulse = Vec3::new(0.0, 6.0, 0.0);
+    }
+}
+
+fn impulse_forward(
+    mut impulses: Query<&mut ExternalImpulse, With<BouncyBall>>,
+    selected: ResMut<Selected>,
+) {
+    if selected.entity.is_some() {
+        let mut impulse = impulses.get_mut(selected.entity.unwrap()).unwrap();
+        impulse.impulse = Vec3::new(0.0, 0.0, -2.5);
+    }
+}
+fn impulse_left(
+    mut impulses: Query<&mut ExternalImpulse, With<BouncyBall>>,
+    selected: ResMut<Selected>,
+) {
+    if selected.entity.is_some() {
+        let mut impulse = impulses.get_mut(selected.entity.unwrap()).unwrap();
+        impulse.impulse = Vec3::new(-2.5, 0.0, 0.0);
+    }
+}
+fn impulse_back(
+    mut impulses: Query<&mut ExternalImpulse, With<BouncyBall>>,
+    selected: ResMut<Selected>,
+) {
+    if selected.entity.is_some() {
+        let mut impulse = impulses.get_mut(selected.entity.unwrap()).unwrap();
+        impulse.impulse = Vec3::new(0.0, 0.0, 2.5);
+    }
+}
+
+fn impulse_right(
+    mut impulses: Query<&mut ExternalImpulse, With<BouncyBall>>,
+    selected: ResMut<Selected>,
+) {
+    if selected.entity.is_some() {
+        let mut impulse = impulses.get_mut(selected.entity.unwrap()).unwrap();
+        impulse.impulse = Vec3::new(2.5, 0.0, 0.0);
+    }
+}
+
+fn update_score (
+    mut query: Query<&mut Text, With<Text>>,
+    scoreboard: Res<ScoreBoard>,
+) {
+    for mut text in query.iter_mut() {
+        if scoreboard.round == 0 {
+            *text = Text::new("Press enter to drop a ball");
+        } else {
+            *text = Text::new(format!("Round: {}\n\nScore: {}\n\nTotal: {}", scoreboard.round, scoreboard.score, scoreboard.total));
+        }
+    }
+}
+
+fn setup_physics(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Scoreboard
+    commands.spawn((
+        Text::new(""),
+        TextFont {
+            font_size: FontSize::Px(14.0),
+            ..Default::default()
+        },
+        Node {
+            position_type: PositionType::Absolute,
+            top: px(12),
+            left: px(12),
+            ..default()
+        },
+    ));
+
+    // 1. Spawn the Camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 10.0, 25.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
+    // 2. Spawn a Light
+    commands.spawn((
+//        DirectionalLight::default(),
+        PointLight {
+            color: Color::from(WHITE),
+            shadow_maps_enabled: true,
+            intensity: 10_000_000.,
+            range: 80.0,
+            radius: 1.0,
+            shadow_depth_bias: 0.2,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 20.0, 10.0),
+    ));
+
+    // Spawn two Static Ground Planes
+    commands.spawn((
+        RigidBody::Fixed,
+        Fence {},
+        Collider::cuboid(2.5, 1.0, 10.0),
+        Mesh3d(meshes.add(Mesh::from(Cuboid::new(5.0, 2.0, 20.0)))),
+        MeshMaterial3d(materials.add(Color::srgb(0.0, 0.0, 1.0))),
+        Transform::from_xyz(2.0, 1.0, 0.0),
+    ));
+
+    commands.spawn((
+        RigidBody::Fixed,
+        Collider::cuboid(12.5, 0.25, 10.0),
+        Mesh3d(meshes.add(Mesh::from(Cuboid::new(25.0, 0.5, 20.0)))),
+        MeshMaterial3d(materials.add(Color::srgb(0.0, 1.0, 0.0))),
+        Transform::from_xyz(2.0, -0.25, 0.0),
+    ));
+
+    // And a cylinder
+    commands.spawn((
+        RigidBody::Fixed,
+        Post {},
+        Collider::cylinder(2.5, 1.0),
+        InheritedVisibility::default(),
+        Mesh3d(meshes.add(Cylinder::new( 1.0, 5.0))),
+        MeshMaterial3d(materials.add(Color::srgb(1.0, 1.0, 0.0))),
+        Transform::from_xyz(2.0, 2.5, 0.0),
+    ));
+    commands.spawn((
+                SensorChild{},
+                 Collider::cylinder(2.5, 1.0),
+                  Sensor,
+                  ActiveEvents::COLLISION_EVENTS,
+                 Transform::from_xyz(2.0, 2.5, 0.0),
+    ));
+}
